d2974bbc17622b1be5b10788882f1c76aaecd8e7
Pavel-V1 <pavelvoronov009@gmail.com>
2025-12-01 01:51:58 +0300

    commit

--- python.py
+++ python.py
@@ -1,1 +1,25 @@
-python python python...
+from math import sqrt
+
+
+def add_numbers(x, y):
+    return x + y
+
+
+def CalculateSquareRoot(Number):
+    return sqrt(Number)
+
+
+def calc(your_number):
+    if your_number <= 0:
+        return
+
+    return f"Мы вычислили квадратный корень из введённого вами числа. \
+            Это будет: {CalculateSquareRoot(your_number)}"
+
+
+x = 10
+y = 5
+
+print("Сумма чисел: ", add_numbers(x, y))
+
+print(calc(25.5))
